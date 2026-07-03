4574627e20f70f7316a769c84235fe72db53cd64
Premnath Anbu <115349159+PremnathAnbu@users.noreply.github.com>
2026-07-03 17:12:49 +0530

    Revise social media links in socials.js
    Updated social media links for LinkedIn, Gmail, and GitHub.

--- src/constants/socials.js
+++ src/constants/socials.js
@@ -7,7 +7,7 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 export const socials = [
   { label: "Twitter / X", href: "https://twitter.com", icon: faXTwitter },
-  { label: "LinkedIn", href: "https://linkedin.com", icon: faLinkedinIn },
-  { label: "Gmail", href: "mailto:premnath@example.com", icon: faEnvelope },
-  { label: "GitHub", href: "https://github.com", icon: faGithub },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/premnathanbu", icon: faLinkedinIn },
+  { label: "Gmail", href: "mailto:contact@premnathanbu.com", icon: faEnvelope },
+  { label: "GitHub", href: "https://github.com/PremnathAnbu", icon: faGithub },
 ];
